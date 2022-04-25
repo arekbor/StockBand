5f6d54409a2bb247e5f2493b87a9ed658a59df20
a_r_e_k97 <arekindependent@mgial.com>
2022-04-25 22:27:35 +0200

    add ckeditor

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StockBand.Controllers
+{
+    [Authorize]
+    public class UploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
