38c2b4ec1d97d89aec8dacc8860bc4f45033dbaa
a_r_e_k97 <arekindependent@mgial.com>
2022-06-03 22:21:12 +0200

    checkout commit

---
+++ Data/TypeOfLibrary.cs
@@ -0,0 +1,8 @@
+﻿namespace StockBand.Data
+{
+    public enum TypeOfLibrary
+    {
+        Track = 0,
+        Album = 1,
+    }
+}
---
+++ ViewModel/LibraryDto.cs
@@ -0,0 +1,10 @@
+﻿using StockBand.Data;
+
+namespace StockBand.ViewModel
+{
+    public class LibraryDto
+    {
+        public string Name { get; set; }
+        public TypeOfLibrary TypeOfLibrary { get; set; }
+    }
+}
