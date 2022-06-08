c330c1d4a26940a4339089bcb82913a5ca4fe726
a_r_e_k97 <arekindependent@mgial.com>
2022-06-08 16:55:31 +0200

    add textCopyFunction js

---
+++ wwwroot/js/copyTextFunction.js
@@ -0,0 +1,10 @@
+﻿function copyText(elementToCopy, elementInfo) {
+    console.log(elementToCopy);
+    elementToCopy.select();
+    elementToCopy.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(elementToCopy.value);
+    elementInfo.style.display = "block";
+    setTimeout(function () {
+        elementInfo.style = "display:none";
+    }, 3500);
+}
